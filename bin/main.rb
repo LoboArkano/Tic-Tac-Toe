4fcd99648aa72b347863dd21480cae70d5776902
LoboArkano <pipiolo_mananes@hotmail.com>
2020-04-23 11:51:40 -0500

    Update main.rb
    Show the input of the users

--- bin/main.rb
+++ bin/main.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby
-p1_name = ''
-p2_name = ''
 p1_choices = []
 p2_choices = []
-input = ''
 
 puts 'Tic Tac Toe Game'
 print 'Player 1 Name: '
@@ -11,12 +8,31 @@ p1_name = gets.chomp
 print 'Player 2 Name: '
 p2_name = gets.chomp
 puts "Let's Start!"
-print 'Player 1 is your turn: '
+print "#{p1_name} is your turn: "
 input = gets.chomp
 p1_choices.push(input)
-puts 'Player 1 choice 3'
-print 'Player 2 is your turn: '
+puts "#{p1_name} choice #{input}"
+print "#{p2_name} is your turn: "
 input = gets.chomp
 p2_choices.push(input)
-puts 'Player 2 choice 1'
-
+puts "#{p2_name} choice #{input}"
+print "#{p1_name} is your turn: "
+input = gets.chomp
+p1_choices.push(input)
+puts "#{p1_name} choice #{input}"
+print "#{p2_name} is your turn: "
+input = gets.chomp
+p2_choices.push(input)
+puts "#{p2_name} choice #{input}"
+print "#{p1_name} is your turn: "
+input = gets.chomp
+p1_choices.push(input)
+puts "#{p1_name} choice #{input}"
+print "#{p2_name} is your turn: "
+input = gets.chomp
+p2_choices.push(input)
+puts "#{p2_name} choice #{input}"
+puts "#{p2_name} won the game"
+puts "#{p2_name} Win the Game!"
+puts 'Congratulation!'
+puts 'You wanna play again?'
