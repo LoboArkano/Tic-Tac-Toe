4135e00558c368761ca8ca8e2ffac3305a44b88a
LoboArkano <pipiolo_mananes@hotmail.com>
2020-04-23 11:15:52 -0500

    Create basic user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
+
+puts 'Tic Tac Toe Game'
+print 'Player 1 Name: '
+print 'Player 2 Name: '
+puts "Let's Start!"
+print 'Player 1 is your turn: '
+puts 'Player 1 choice 3'
+print 'Player 2 is your turn: '
+puts 'Player 2 choice 1'
