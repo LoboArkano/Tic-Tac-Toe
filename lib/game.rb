cfa3ee5f9269e8d3867f3dd8dfe826802b3f16bb
LoboArkano <pipiolo_mananes@hotmail.com>
2020-04-27 15:27:20 -0500

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -67,6 +67,10 @@ class Rules
     @victory_rows = [%w[1 2 3], %w[4 5 6], %w[7 8 9], %w[1 4 7], %w[2 5 8], %w[3 6 9], %w[1 5 9], %w[3 5 7]]
   end
 
+  def in_range?(choice)
+    choice.to_i >= 1 && choice.to_i <= 9
+  end
+
   def untaken?(choice, opponent_choices)
     arr = []
     arr.push(choice)
@@ -75,15 +79,17 @@ class Rules
     true
   end
 
-  def valid_move(choice, opponent_choices)
-    (choice.to_i >= 1 && choice.to_i <= 9) && untaken?(choice, opponent_choices)
+  def previous_choice?(choice, choices)
+    !choices.include?(choice)
+  end
+
+  def valid_move(choice, choices, opponent_choices)
+    in_range?(choice) && untaken?(choice, opponent_choices) && previous_choice?(choice, choices)
   end
 
   def win_check(choices)
     @victory_rows.each do |victory_row|
-      if victory_row.to_set.subset?(choices.to_set)
-        return true
-      end
+      return true if victory_row.to_set.subset?(choices.to_set)
     end
     false
   end
