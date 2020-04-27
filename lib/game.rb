a980a6eee8d37d2084ac89a9abbd3b3de60ffce4
LoboArkano <pipiolo_mananes@hotmail.com>
2020-04-27 10:53:13 -0500

    Update update_move method
    Create two methods

--- lib/game.rb
+++ lib/game.rb
@@ -25,21 +25,29 @@ class Board
   end
 
   def update_move(choice, sign)
-    i = if choice / 3.0 <= 1
-          0
-        elsif choice / 3.0 <= 2
-          2
-        else
-          4
-        end
-    j = if choice % 3 == 1
-          0
-        elsif choice % 3 == 2
-          2
-        else
-          4
-        end
-    @board[i][j] = sign
+    i = row_position
+    j = column_position
+    @board[i][j] = sign if valid(choice)
+  end
+
+  def row_position(choice)
+    if choice / 3.0 <= 1
+      0
+    elsif choice / 3.0 <= 2
+      2
+    else
+      4
+    end
+  end
+
+  def column_position(choice)
+    if choice % 3 == 1
+      0
+    elsif choice % 3 == 2
+      2
+    else
+      4
+    end
   end
 end
 
