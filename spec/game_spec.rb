f685600e3dc4c8c6e52433be00b4a9a9a92c05c5
LoboArkano <pipiolo_mananes@hotmail.com>
2020-05-04 15:28:54 -0500

    Create test for untaken? method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,8 @@
 require './lib/game'
 
 describe Game do
+  let(:game) { Game.new }
   describe '#in_range?' do
-    let(:game) { Game.new }
     it 'Return true if input is greater or equal than 1 and less or equal than 9' do
       expect(game.in_range?(5)).to eql(true)
     end
@@ -11,6 +11,15 @@ describe Game do
     end
     it 'Return false if input is not a number' do
       expect(game.in_range?('test')).to eql(false)
+    end
+  end
+  describe '#untaken?' do
+    let(:opponent_choices) { [2, 5, 7, 9] }
+    it 'Return true if the input is not equal to one of the opponent choices' do
+      expect(game.untaken?(6, opponent_choices)).to eql(true)
+    end
+    it 'Return false if the input is equal to one of the opponent choices' do
+      expect(game.untaken?(7, opponent_choices)).to eql(false)
     end
   end
 end
