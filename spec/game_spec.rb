6f0092b036209c3b7fea429839f64e31ee403ce4
LoboArkano <pipiolo_mananes@hotmail.com>
2020-05-04 15:15:53 -0500

    Create test for in_range? method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,16 @@
+require './lib/game'
 
+describe Game do
+  describe '#in_range?' do
+    let(:game) { Game.new }
+    it 'Return true if input is greater or equal than 1 and less or equal than 9' do
+      expect(game.in_range?(5)).to eql(true)
+    end
+    it 'Return false if input is less than 1 or greater than 9' do
+      expect(game.in_range?(10)).to eql(false)
+    end
+    it 'Return false if input is not a number' do
+      expect(game.in_range?('test')).to eql(false)
+    end
+  end
+end
