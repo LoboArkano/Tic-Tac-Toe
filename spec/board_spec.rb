ea343b56521798dc96d0e9f878fed21325399b9a
LoboArkano <pipiolo_mananes@hotmail.com>
2020-05-05 23:52:03 -0500

    Create test for example method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,15 @@
 require './lib/board'
+
+describe Board do
+  let(:game_board) { Board.new }
+  describe '#example' do
+    it "Insert 'X' and 'O' in different places" do
+      game_board.example
+      expect(game_board.board[0][0]).to eql('X')
+      expect(game_board.board[2][2]).to eql('X')
+      expect(game_board.board[4][4]).to eql('X')
+      expect(game_board.board[2][0]).to eql('O')
+      expect(game_board.board[2][4]).to eql('O')
+    end
+  end
+end
